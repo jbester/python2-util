f2a63b2215c4459f88be6750fa8590e16d190c44
Jeffrey Bester <jbester@gmail.com>
2008-12-31 21:47:44 -0500

    added disutils setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(name = "pyutil",
+    version = "1",
+    description = "Python Utility Library",
+    author = "J. Bester",
+    author_email = "code@freshlime.org",
+    url = "http://github.com/jbester/python-util/",
+    #Name the folder where your packages live:
+    #(If you have other packages (dirs) or modules (py files) then
+    #put them into the package directory - they will be found 
+    #recursively.)
+    packages = ['pyutil']
+) 
